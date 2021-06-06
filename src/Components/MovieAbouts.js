24ced034d741f1ac7197921771ca51308306f013
Oussema Selmi <oussemaselmi1901@gmail.com>
2021-06-06 11:14:44 +0100

    added_home_button

--- src/Components/MovieAbouts.js
+++ src/Components/MovieAbouts.js
@@ -1,4 +1,6 @@
 import React,{useState,useEffect} from 'react'
+import {FiHome} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
 
 const MovieAbouts = ({movies, match}) => {
     const [movie, setMovie] = useState({});
@@ -7,6 +9,9 @@ const MovieAbouts = ({movies, match}) => {
     setMovie(movies.find((el) => el.id === +match.params.movieid));},[movies, match.params.movieid]);
   return (
     <div className="MovieAbouts">
+      <Link to="/">
+        <FiHome style={{height:"50px",width:"50px"}} />
+      </Link>
         <h1 className="moviename">{movie.title}</h1>
         <iframe title={movie.title} width="50%" height="500px" src={movie.trailer}/>
         <br />
